498bba23dd97ebf2d5a5f1c4d409477ee8a1f348
Jens Peter Secher <jpsecher@gmail.com>
2019-01-21 17:46:41 +0100

    Use Bit Value from library.

--- blinky/main.c
+++ blinky/main.c
@@ -11,21 +11,21 @@ int main () {
 	pin_as_output(PB4);
   while (1) {
     pin_on(PB4);
-    _delay_ms(500);
+    _delay_ms(1000);
     pin_off(PB4);
-    _delay_ms(500);
+    _delay_ms(1000);
   }
   return 0;
 }
 
 void pin_as_output (uint8_t pin) {
-  DDRB |= (1 << pin);
+  DDRB |= _BV(pin);
 }
 
 void pin_on (uint8_t pin) {
-  PORTB |= (1 << pin);
+  PORTB |= _BV(pin);
 }
 
 void pin_off (uint8_t pin) {
-  PORTB &= ~(1 << pin);
+  PORTB &= ~_BV(pin);
 }
