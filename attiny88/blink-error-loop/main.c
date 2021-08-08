13701a1ee919fb30f38931a74cda75ba495ddb5a
Jens Peter Secher <jpsecher@gmail.com>
2021-08-08 21:17:25 +0200

    Not working: function pointers

--- attiny88/blink-error-loop/main.c
+++ attiny88/blink-error-loop/main.c
@@ -26,6 +26,14 @@
 #define SHORT_BLINK_ms 50
 #define LONG_BLINK_ms 450
 
+// H_buttons_and_encoders data 1
+#define OUTPUT_ENABLE 0b10000000
+#define SET_VOLTAGE   0b01000000
+#define SET_CURRENT   0b00100000
+#define VOLTAGE_DOWN  0b00010000
+#define VOLTAGE_UP    0b00001000
+#define CURRENT_DOWN  0b00000100
+#define CURRENT_UP    0b00000010
 
 enum Status {
   // Non-errors.
@@ -33,8 +41,12 @@ enum Status {
   S_processing_needed,
   // Errors.
   S_memory_layout_error,
-  S_unknown_error,
-  S_end_marker
+  S_unknown_error
+};
+
+enum Intr_Handler {
+  H_sanity,
+  H_buttons_and_encoders
 };
 
 #define N_INTR_HANDLERS 3
@@ -44,14 +56,10 @@ uint8_t intr_handler_to_data_index_H [N_INTR_HANDLERS];
 #define N_INTR_TOTAL_BYTES 5
 uint8_t intr_handler_data [N_INTR_TOTAL_BYTES];
 
-enum Intr_Handler {
-  H_sanity,
-  H_my_handler,
-  H_other_handler
-};
+typedef void (*handler_t)(void);
+handler_t intr_handler_H [N_INTR_HANDLERS];
 
 #define INLINE static inline
-
 INLINE void ui_init (void);
 INLINE void init_intr_handler_tables (void);
 INLINE void set_status_H_S (uint8_t handler, uint8_t status);
@@ -60,10 +68,10 @@ INLINE uint8_t get_status_H (uint8_t handler);
 INLINE uint8_t get_data_H_n (uint8_t handler, uint8_t index);
 INLINE void init_intr_handler_data (void);
 INLINE void sanity_check (void);
-INLINE void simulate_error (void);
 INLINE bool error_present (void);
 INLINE void show_all_errors (void);
 INLINE void show_error (uint8_t status);
+INLINE void process_all_inputs (void);
 INLINE void turn_off_output (void);
 INLINE void pin_as_output_A (uint8_t pin);
 INLINE void pin_on_A (uint8_t pin);
@@ -72,32 +80,46 @@ INLINE void pin_as_input_with_pull_up_D (uint8_t pin);
 INLINE void setup_irq_on_buttons_and_encoders (void);
 INLINE void pcint_23_to_16_generate_interupt_on_pci2 (void);
 INLINE void enable_interupt_from_buttons_and_encoders (void);
+INLINE void init_sanity_handler (uint8_t handler);
+INLINE void init_buttons_and_enoders_handler (uint8_t handler);
 
+// Functions tied to specific Intr_Handlers.
+void unknown_error (void);
+void simulate_error (void);
+void h_buttons_and_encoders (void);
 
 int main () {
   init_intr_handler_tables();
   ui_init();
   setup_irq_on_buttons_and_encoders();
   sanity_check();
-  // simulate_error();  // <-------
   while (1) {
     while (error_present()) {
       turn_off_output();
       show_all_errors();
       _delay_ms(LONG_BLINK_ms);
     }
-    // Normal processing...
-    if (bit_is_clear(PIND, D_OE_SW))
-      pin_on_A(A_CV_LED);
-    else
-      pin_off_A(A_CV_LED);
+    process_all_inputs();
   }
   return 0;
 }
 
-void simulate_error (void) {
-  set_status_H_S(H_sanity, S_memory_layout_error);
-  set_status_H_S(H_other_handler, S_unknown_error);
+void process_all_inputs (void) {
+  uint8_t handler = 0;
+  uint8_t status;
+  for (; handler < N_INTR_HANDLERS; ++handler) {
+    cli();
+    status = get_status_H(handler);
+    set_status_H_S(handler, S_ok);
+    sei();
+    if (status == S_processing_needed)
+      //simulate_error(); // <---
+      intr_handler_H[handler]();
+  }
+  // if (bit_is_clear(PIND, D_OE_SW))
+  //   pin_on_A(A_CV_LED);
+  // else
+  //   pin_off_A(A_CV_LED);
 }
 
 bool error_present (void) {
@@ -158,11 +180,24 @@ void sanity_check (void) {
 }
 
 void init_intr_handler_tables (void) {
-  intr_handler_data_layout_H[H_sanity] = 1;  // status byte for sanity check
-  intr_handler_data_layout_H[H_my_handler] = 3;  // my_intr_handler uses status byte and two value bytes
-  intr_handler_data_layout_H[H_other_handler] = 1;  // another_intr_handler only uses status byte
+  init_sanity_handler(H_sanity);
+  init_buttons_and_enoders_handler(H_buttons_and_encoders);
   init_intr_handler_data();
 };
+
+void init_sanity_handler (uint8_t handler) {
+  // Only status byte for sanity check.
+  intr_handler_data_layout_H[handler] = 1;
+  intr_handler_H[handler] = unknown_error;
+}
+
+void init_buttons_and_enoders_handler (uint8_t handler) {
+  // Status byte and a bytes for the three buttons (3 bits) and direction of
+  // the two encoders (2 * 2 bits).
+  intr_handler_data_layout_H[H_buttons_and_encoders] = 2;
+  //intr_handler_H[H_buttons_and_encoders] = unknown_error;  // XXX
+  intr_handler_H[H_buttons_and_encoders] = simulate_error;  // XXX
+}
 
 void init_intr_handler_data (void) {
   uint8_t handler = 0;
@@ -218,21 +253,51 @@ void pin_as_input_with_pull_up_D (uint8_t pin) {
 }
 
 void setup_irq_on_buttons_and_encoders (void) {
-    pcint_23_to_16_generate_interupt_on_pci2();
-    enable_interupt_from_buttons_and_encoders();
-    sei();
+  pcint_23_to_16_generate_interupt_on_pci2();
+  enable_interupt_from_buttons_and_encoders();
+  sei();
 }
 
 void pcint_23_to_16_generate_interupt_on_pci2 (void) {
-    PCICR |= _BV(PCIE2);
+  PCICR |= _BV(PCIE2);
 }
 
 void enable_interupt_from_buttons_and_encoders (void) {
-    PCMSK2 |= 0b11111111;
+  PCMSK2 |= 0b11111111;
 }
 
+#define OUTPUT_ENABLE 0b10000000
+#define SET_VOLTAGE   0b01000000
+#define SET_CURRENT   0b00100000
+#define VOLTAGE_DOWN  0b00010000
+#define VOLTAGE_UP    0b00001000
+#define CURRENT_DOWN  0b00000100
+#define CURRENT_UP    0b00000010
+
+
+void unknown_error (void) {
+  set_status_H_S(H_sanity, S_unknown_error);
+}
+
+void simulate_error (void) {
+  set_status_H_S(H_sanity, S_memory_layout_error);
+  set_status_H_S(H_buttons_and_encoders, S_unknown_error);
+}
+
+//
+// Buttons & Encoders
+//
+
 ISR (PCINT2_vect) {
+  uint8_t state = 0;
   // IO pulled down means button pressed.
   if (bit_is_clear(PIND, D_OE_SW))
-    set_status_H_S(H_other_handler, S_end_marker);
+    state |= OUTPUT_ENABLE;
+  // TODO: Rest of button...
+  set_data_H_n_b(H_buttons_and_encoders, 1, state);
+  set_status_H_S(H_buttons_and_encoders, S_processing_needed);
+}
+
+void h_buttons_and_encoders (void) {
+
 }
