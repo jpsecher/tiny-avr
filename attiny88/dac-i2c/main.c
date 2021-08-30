2627dadffdf165a67cdc6f0c2ec32e14c3e80178
Jens Peter Secher <jpsecher@gmail.com>
2021-08-30 22:23:49 +0200

    Prepare for DAC i2C

--- attiny88/dac-i2c/main.c
+++ attiny88/dac-i2c/main.c
@@ -2,15 +2,16 @@
 // Connections
 // ===========
 //
-// All buttons & encoder use PCI2 (pins PDx).
-// LEDs on PAx.
-//
+// All buttons & encoder use PCI0 (pins PBx).
+// LEDs on PDx.
+// DAC on PC4/5.
 
-// PA0 --- +Red LED- --- GND
-#define A_CC_LED PA0
 
-// PA1 --- +Green LED- --- GND
-#define A_CV_LED PA1
+//  PD6 --- +LED(R)- --- GND
+#define D_CC_LED PD6
+
+//  PD7 --- +LED(G)- --- GND
+#define D_CV_LED PD7
 
 // PB2 --- SW_PUSH --- GND
 // PD2 --- SW_PUSH --- GND
@@ -47,14 +48,13 @@ enum Status {
 enum Intr_Handler {
   H_sanity,
   H_button,
-  H_dummy
 };
 
-#define N_INTR_HANDLERS 3
+#define N_INTR_HANDLERS 2
 uint8_t intr_handler_data_layout_H [N_INTR_HANDLERS];
 uint8_t intr_handler_to_data_index_H [N_INTR_HANDLERS];
 
-#define N_INTR_TOTAL_BYTES 4
+#define N_INTR_TOTAL_BYTES 3
 uint8_t intr_handler_data [N_INTR_TOTAL_BYTES];
 
 typedef void (*handler_t)(void);
@@ -73,9 +73,9 @@ INLINE bool error_present (void);
 INLINE void show_all_errors (void);
 INLINE void show_error (uint8_t status);
 INLINE void process_all_events (void);
-INLINE void pin_as_output_A (uint8_t pin);
-INLINE void pin_on_A (uint8_t pin);
-INLINE void pin_off_A (uint8_t pin);
+INLINE void pin_as_output_D (uint8_t pin);
+INLINE void pin_on_D (uint8_t pin);
+INLINE void pin_off_D (uint8_t pin);
 INLINE void pin_as_input_with_pull_up_B (uint8_t pin);
 INLINE void setup_irq_on_button (void);
 INLINE void pcint_7_to_0_generate_interupt_on_pci0 (void);
@@ -85,7 +85,6 @@ INLINE void init_button_handler (uint8_t handler);
 
 // Functions tied to specific Intr_Handlers.
 void unknown_error (void);
-void simulate_error (void);
 void h_button (void);
 
 int main () {
@@ -136,22 +135,22 @@ void show_all_errors (void) {
 }
 
 void show_error (uint8_t status) {
-  pin_off_A(A_CV_LED);
+  pin_off_D(D_CV_LED);
   // Blink red to signal error.
-  pin_on_A(A_CC_LED);
+  pin_on_D(D_CC_LED);
   _delay_ms(LONG_BLINK_ms);
-  pin_off_A(A_CC_LED);
+  pin_off_D(D_CC_LED);
   _delay_ms(LONG_BLINK_ms);
   while (status != 0) {
     // Blink green, short for 0, long for 1.
-    pin_on_A(A_CV_LED);
+    pin_on_D(D_CV_LED);
     if ((status & 1) == 1) {
       _delay_ms(LONG_BLINK_ms);
-      pin_off_A(A_CV_LED);
+      pin_off_D(D_CV_LED);
     }
     else {
       _delay_ms(SHORT_BLINK_ms);
-      pin_off_A(A_CV_LED);
+      pin_off_D(D_CV_LED);
     }
     _delay_ms(LONG_BLINK_ms);
     status >>= 1;
@@ -172,7 +171,6 @@ void sanity_check (void) {
 void init_intr_handler_tables (void) {
   init_sanity_handler(H_sanity);
   init_button_handler(H_button);
-  init_sanity_handler(H_dummy); // same as sanity
   init_intr_handler_data();
 };
 
@@ -180,6 +178,10 @@ void init_sanity_handler (uint8_t handler) {
   // Only status byte.
   intr_handler_data_layout_H[handler] = 1;
   intr_handler_H[handler] = unknown_error;
+}
+
+void unknown_error (void) {
+  set_status_H_S(H_sanity, S_unknown_error);
 }
 
 void init_button_handler (uint8_t handler) {
@@ -215,21 +217,21 @@ uint8_t get_data_H_n (uint8_t handler, uint8_t index) {
 }
 
 void ui_init (void) {
-  pin_as_output_A(A_CC_LED);
-  pin_as_output_A(A_CV_LED);
+  pin_as_output_D(D_CC_LED);
+  pin_as_output_D(D_CV_LED);
   pin_as_input_with_pull_up_B(B_OE_SW);
 }
 
-void pin_as_output_A (uint8_t pin) {
-  DDRA |= _BV(pin);
+void pin_as_output_D (uint8_t pin) {
+  DDRD |= _BV(pin);
 }
 
-void pin_on_A (uint8_t pin) {
-  PORTA |= _BV(pin);
+void pin_on_D (uint8_t pin) {
+  PORTD |= _BV(pin);
 }
 
-void pin_off_A (uint8_t pin) {
-  PORTA &= ~_BV(pin);
+void pin_off_D (uint8_t pin) {
+  PORTD &= ~_BV(pin);
 }
 
 void pin_as_input_with_pull_up_B (uint8_t pin) {
@@ -258,8 +260,9 @@ void enable_interupt_from_buttons (void) {
 
 ISR (PCINT0_vect) {
   // IO pulled down means button pressed.
-  if (bit_is_clear(PINB, B_OE_SW))
+  if (bit_is_clear(PINB, B_OE_SW)) {
     set_data_H_n_b(H_button, 1, 0xFF);
+  }
   set_status_H_S(H_button, S_processing_needed);
 }
 
@@ -270,17 +273,12 @@ void h_button (void) {
   status = get_status_H(H_button);
   state = get_data_H_n(H_button, 1);
   set_status_H_S(H_button, S_ok);
+  set_data_H_n_b(H_button, 1, 0x00);
   sei();
-  if (status == S_processing_needed)
+  if (status == S_processing_needed) {
     if (state)
-      simulate_error();
-}
-
-void simulate_error (void) {
-  set_status_H_S(H_sanity, S_memory_layout_error);
-  set_status_H_S(H_dummy, S_unknown_error);
-}
-
-void unknown_error (void) {
-  set_status_H_S(H_sanity, S_unknown_error);
+      pin_on_D(D_CV_LED);
+    else
+      pin_off_D(D_CV_LED);
+  }
 }
